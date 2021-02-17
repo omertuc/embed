06dacab6be42a60037156fcbd880b10e09b75e7a
Omer Tuchfeld <otuchfel@redhat.com>
2021-02-17 14:48:33 +0200

    test

--- b/go.mod
+++ b/go.mod
@@ -1,3 +1,5 @@
 module github.com/omertuc/embed/b
 
 go 1.15
+
+require github.com/omertuc/embed/a v0.0.0-20210217123240-2dad2ab47d10 // indirect
--- b/main.go
+++ b/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "github.com/omertuc/embed/a/main"
+import "github.com/omertuc/embed/a"
 
 func main() {
 	fmt.Println(main.S)
